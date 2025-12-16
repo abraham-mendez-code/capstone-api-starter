1a173a38862975c93300ae97983073672b911389
abraham-mendez-code <abraham.me001@gmail.com>
2025-12-16 08:38:55 -0500

    feat(profile): add ProfileController to handle /profile endpoints
    Added ProfileController with constructor-based dependency injection for ProfileDao and UserDao.
    
    Related #18

---
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -0,0 +1,30 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.Profile;
+
+@RestController
+@RequestMapping("profile")
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
+public class ProfileController {
+
+    // CLASS ATTRIBUTES
+    private ProfileDao profileDao;
+    private UserDao userDao;
+
+    @Autowired
+    public ProfileController(ProfileDao profileDao, UserDao userDao) {
+        this.profileDao = profileDao;
+        this.userDao = userDao;
+    }
+
+
+}
